4df1785c447a0129ec50029c5e7acb86efe6f9ea
Md. Shohidujjaman Sajib <sajib1615018@stud.kuet.ac.bd>
2023-02-27 22:11:14 +0600

    product action,reducer ok

---
+++ src/redux/product/actions.js
@@ -0,0 +1,14 @@
+import { ADD } from "./actionTypes";
+
+export const addProduct = (product) => {
+  return {
+    type: ADD,
+    payload: {
+      ...product,
+      // name
+      // category
+      // image URL
+      // price
+    },
+  };
+};
---
+++ src/redux/product/productReducer.js
@@ -0,0 +1,31 @@
+import { ADD } from "./actionTypes";
+const initialState = [];
+
+function getNewID(state) {
+  if (state?.length > 0) {
+    return state.reduce(
+      (currentMax, object) => Math.max(currentMax, object.id),
+      -1
+    );
+  }
+  return 0;
+}
+
+export default function productReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        {
+          id: getNewID(state),
+          name: action.payload.name,
+          category: action.payload.category,
+          imageURL: action.payload.imageURL,
+          price: action.payload.price,
+        },
+      ];
+
+    default:
+      return state;
+  }
+}
